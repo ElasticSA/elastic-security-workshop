a46734f39669b97d07840e8effcca39500f1b55b
Thorben Jändling <thorbenj@users.noreply.github.com>
2020-07-30 19:32:28 +0200

    first pass at adjusting scripts to be standalone

--- files/install_openssh.ps1
+++ files/install_openssh.ps1
@@ -6,9 +6,6 @@
 # Open the SSH server to the world?
 $enable_fw_rule = "False"
 
-$workdir = New-Item -ItemType Directory -Path (Join-Path $env:TEMP ([System.Guid]::NewGuid()))
-echo "*** workdir = $workdir"
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Get Win-OpenSSH download url; thanks: https://github.com/PowerShell/Win32-OpenSSH/wiki/How-to-retrieve-links-to-latest-packages
@@ -18,21 +15,17 @@ $response=$request.GetResponse()
 $wossh_url = $([String]$response.GetResponseHeader("Location")).Replace('tag','download') + '/OpenSSH-Win64.zip' 
 
 echo "Downloading $wossh_url"
-Invoke-WebRequest -Uri $wossh_url -OutFile "$workdir\OpenSSH-Win64.zip"
+Invoke-WebRequest -Uri $wossh_url -OutFile "$pwd\OpenSSH-Win64.zip"
 
 echo "Extracting files"
-Expand-Archive -Path "$workdir\OpenSSH-Win64.zip" -DestinationPath "C:\Program Files\" -Force
+Expand-Archive -Path "$pwd\OpenSSH-Win64.zip" -DestinationPath "C:\Program Files\" -Force
 
 echo "Installing service"
 powershell.exe -ExecutionPolicy Bypass -File "C:\Program Files\OpenSSH-Win64\install-sshd.ps1"
 
 echo "Adding firewall"
-New-NetFirewallRule -Name sshd0 -DisplayName 'OpenSSH Server (sshd)' -Enabled $enable_fw_rule -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+New-NetFirewallRule -Name sshd_any -DisplayName 'OpenSSH Server (sshd)' -Enabled $enable_fw_rule -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 echo "Starting service"
 Set-Service sshd -StartupType Automatic
 Start-Service -Name sshd
-
-#$ps_location = (Get-Command powershell.exe | Select-Object -ExpandProperty Definition)
-#echo "Setting ssh shell to $ps_location"
-#Set-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "$ps_location" -Type String
